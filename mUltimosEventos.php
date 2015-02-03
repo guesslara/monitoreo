68b715be3aa9f1fca17d5d0c2a633af90fe25c9e
Gerardo Lara <gerardolara1984@gmail.com>
2015-02-03 00:33:10 -0600

    Ajustes

---
+++ mUltimosEventos.php
@@ -0,0 +1,21 @@
+<?php
+  if($_SERVER["HTTP_REFERER"]==""){//se valida si viene desde la pagina o se invoco directamente
+    header("Location: errors/index.php?e=error404");
+    exit();
+  }else{
+    session_start();
+    if(!isset($_SESSION["s_id_4togo"])){
+      echo "Error";
+      exit();
+    }else{
+      echo "<pre>";
+      print_r($_SESSION);
+      echo "</pre>";
+      $idUsuario=$_SESSION["s_id_4togo"];
+      include "claseMonitoreo.php";
+      $objM2=new monitoreo();
+      $objM2->verUltimosEventos($idUsuario);
+
+    }
+  }
+?>
